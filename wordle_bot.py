3d312ace944bb1fe16d281fd0820e571c7f87864
dmddjack <dmddjack@gmail.com>
2022-11-11 11:42:00 +0800

    fix: improve performance of greedy algo. modify function parameters. add crete_greedy() to store algo result into JSON files. add: function bot() to provide suggestion to each input of a game.

--- wordle_bot.py
+++ wordle_bot.py
@@ -1,9 +1,10 @@
 import json
 import math
+from time import time, gmtime, strftime
 
 import numpy as np
 
-import wordle_json
+import wordle_json as wj
 
 
 def info(p: float | np.ndarray) -> float:
@@ -28,22 +29,27 @@ def entropy(pmf: list[float]) -> float:
     return result
 
 
-def eliminate(guess: str, pattern: str, n: int = 0, write: bool = True) -> None | dict:
+def eliminate(guess: str, pattern: str, file: int | dict = 0, write: bool = True) -> None | dict:
     """Eliminate words that does not comply with the guess and the pattern. n is the number of times of attempt.
     store the result in a new txt file, and create the corresponding JSON file."""
+    if file == int:
+        with open(f"input_mass_function_{file - 1}.json", "r") as in_f:
+            words = json.load(in_f)[guess][pattern]
+    else:
+        write = False
+        words = file[guess][pattern]
+    print(f"data size: {len(words)}")
+    # print(f"Number of remaining choices: {len(words)}")
+    if write:
+        with open(f"word_list_{file}.txt", "w") as out_f:
+            for word in words:
+                print(word, file=out_f)
+        wj.create_data(file)
+    else:
+        return wj.create_data(np.array(words))
 
-    with open(f"input_mass_function_{n - 1}.json", "r") as in_f:
-        words = json.load(in_f)[guess][pattern]
-        if write:
-            with open(f"word_list_{n}.txt", "w") as out_f:
-                for word in words:
-                    print(word, file=out_f)
-            wordle_json.create_data(n)
-        else:
-            return wordle_json.create_data(np.array(words))
 
-
-def one_step_greedy(disp: int = 10, n: int = 0, data=None) -> list[tuple]:
+def one_step_greedy(n: int = 0, disp: int = 10, data=None) -> list[tuple]:
     """Use the greedy algorithm to find the maximum entropy and the corresponding word in a single step.
     disp indicates the number of items to be returned. the whole list is returned if disp == -1.
     n is the number of times of attempt. Return a list of words and entropy in descending order."""
@@ -64,28 +70,69 @@ def one_step_greedy(disp: int = 10, n: int = 0, data=None) -> list[tuple]:
     return entropy_list[:disp] if disp != -1 else entropy_list
 
 
-def two_step_greedy(disp: int = 10, n: int = 0) -> list[tuple]:
+def two_step_greedy(n: int = 0, disp: int = 10, data=None) -> list[tuple]:
     """Use the greedy algorithm to find the maximum entropy and the corresponding word in two steps.
     disp indicates the number of items to be returned. the whole list is returned if disp == -1.
     n is the number of times of attempt. Return a list of words and sum of entropy of two steps in descending order."""
-    with open(f"pmfs_{n}.json", "r") as f1:
-        pmfs = json.load(f1)
-        entropy_list = []
-        total, count = len(pmfs), 0
+    start_time = time()
+    if data is None:
+        with open(f"pmfs_{n}.json", "r") as f1, open(f"input_mass_function_{n}.json", "r") as f2:
+            pmfs = json.load(f1)
+            mass_func = json.load(f2)
 
-        for guess, pmf in pmfs.items():
-            sec_ord_entropy = 0  # define the entropy at the second step
-            for pattern, p in pmf.items():  # find maximum entropy at the second step
-                sub_pmfs = eliminate(guess, pattern, n + 1, write=False)
-                sec_ord_entropy += pmfs[guess][pattern] * one_step_greedy(1, n + 1, sub_pmfs)[0][1]
-                # weighted average of entropy
-            entropy_list.append((guess, entropy(pmf.values()), sec_ord_entropy,
-                                 entropy(pmf.values()) + sec_ord_entropy))
-            count += 1
-            print(f"Progress: {count}/{total} ({count / total * 100}%)")
-        entropy_list.sort(key=lambda x: x[-1], reverse=True)
-        return entropy_list[:disp] if disp != -1 else entropy_list
+    entropy_list = []
+    total = 100  # maximum number of top words in one_step greedy to speed up the program.
+    count = 0
+    top_words = dict(one_step_greedy(n + 1, total, pmfs)).keys()
+    for guess, pmf in pmfs.items():
+        if guess not in top_words:
+            continue
+        sec_ord_entropy = 0  # define the entropy at the second step
+        for pattern, p in pmf.items():  # find maximum entropy at the second step
+            sub_pmfs = eliminate(guess, pattern, file=mass_func, write=False)
+            sec_ord_entropy += pmfs[guess][pattern] * one_step_greedy(n + 1, 1, sub_pmfs)[0][1]
+            # weighted average of entropy
+        entropy_list.append((guess, entropy(pmf.values()) + sec_ord_entropy))
+        count += 1
+        print(f"Progress: {count}/{total} ({count / total * 100}%)")
+        print(f"Time elapsed: {strftime('%H:%M:%S', gmtime(time() - start_time))}")
+        print(f"Estimated time: {strftime(f'%H:%M:%S', gmtime((time() - start_time) / (count / total)))}")
+    entropy_list.sort(key=lambda x: x[-1], reverse=True)
+    return entropy_list[:disp] if disp != -1 else entropy_list
+
+
+def create_greedy() -> None:
+    """Store the initial result of one_step_greedy and two_step_greedy in the corresponding JSON file
+    to speed up the program."""
+    # wj.create_data()
+    one_step = one_step_greedy(disp=-1)
+    with open("one_step_entropy.json", "w") as f:
+        json.dump(dict(one_step), f, indent=4)
+        print("one step done.")
+    two_step = two_step_greedy(disp=-1)
+    with open("two_step_entropy.json", "w") as f:
+        json.dump(dict(two_step), f, indent=4)
+        print("two step done.")
+
+
+def bot() -> None:
+    print(one_step_greedy())
+    for i in range(1, 7):
+        guess = input("Please input your guess:")
+        pattern = input("please input the returned pattern:")
+        if pattern == '22222':
+            for i in range(1, i):
+                wj.del_data(i)
+            break
+        eliminate(guess, pattern, i)
+        print(one_step_greedy(i))
+
+    print("fin.")
+
+
+def simulator(start_word="tares") -> None:
+    pass
 
 
 if __name__ == "__main__":
-    print(two_step_greedy())
+    create_greedy()
